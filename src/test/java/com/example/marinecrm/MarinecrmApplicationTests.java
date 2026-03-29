e6612cbcf4dea5a42a76ea014cec6cd25ce8f05b
gustavo <gustavobroechl25@gmail.com>
2026-03-29 17:55:03 -0300

    chore: add Maven wrapper and test scaffolding

---
+++ src/test/java/com/example/marinecrm/MarinecrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marinecrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarinecrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
