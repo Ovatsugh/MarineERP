243ef496888b1746d83f52b8a53110abc9046d49
gustavo <gustavobroechl25@gmail.com>
2026-03-29 17:54:43 -0300

    chore: configure project setup and environment

---
+++ src/main/java/com/example/marinecrm/MarinecrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.marinecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarinecrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarinecrmApplication.class, args);
+    }
+
+}
